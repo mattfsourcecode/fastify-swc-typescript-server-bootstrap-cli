e2e9ffd76e7a6687bb15f3ea134fe630985fd5b9
Matt Fagan <24415914+matt-development-work@users.noreply.github.com>
2023-11-25 11:50:26 -0500

    Optimize tests

--- tests/init.tests.js
+++ tests/init.tests.js
@@ -6,22 +6,25 @@ const rimraf = require("rimraf");
 
 describe("init.js script", () => {
   const projectName = "test-project";
-  const projectDir = path.join(__dirname, projectName);
+  const tempProjectDirectory = path.join(__dirname, projectName);
 
   // Function to execute the init.js script
   const executeInitScript = (name = projectName) => {
-    // Run the script in the directory above projectDir
+    // Run the script in the directory above tempProjectDirectory
     const cmd = `node ${path.join(__dirname, "..", "init.js")} ${name}`;
-    return execSync(cmd, { encoding: "utf8", cwd: path.dirname(projectDir) });
+    return execSync(cmd, {
+      encoding: "utf8",
+      cwd: path.dirname(tempProjectDirectory),
+    });
   };
 
-  const cleanupProjectDir = () => {
-    if (fs.existsSync(projectDir)) {
-      rimraf.sync(projectDir);
+  const removeTempProjectDirectory = () => {
+    if (fs.existsSync(tempProjectDirectory)) {
+      rimraf.sync(tempProjectDirectory);
     }
   };
 
-  before(cleanupProjectDir);
+  before(removeTempProjectDirectory);
 
   it("should exit with an error if no project name is provided", () => {
     try {
@@ -35,22 +38,18 @@ describe("init.js script", () => {
   it("should successfully clone the repository", () => {
     try {
       executeInitScript();
-
       // Check if the project directory exists
-      expect(fs.existsSync(projectDir)).to.be.true;
-
+      expect(fs.existsSync(tempProjectDirectory)).to.be.true;
       // Check for specific files/directories
-      const expectedFile = path.join(projectDir, "package.json");
+      const expectedFile = path.join(tempProjectDirectory, "package.json");
       expect(fs.existsSync(expectedFile)).to.be.true;
-
       // Optionally, run 'git status' or 'git log' and check the output
       const gitStatus = execSync("git status", {
-        cwd: projectDir,
+        cwd: tempProjectDirectory,
         encoding: "utf8",
       });
       expect(gitStatus).to.include("On branch");
-
-      // You can also check for the latest commit or other git details if needed
+      // Optional: Check for the latest commit or other git details if needed
     } catch (error) {
       console.error("Error:", error);
       throw error;
@@ -58,28 +57,33 @@ describe("init.js script", () => {
   });
 
   it("should navigate to the cloned directory and remove .git directory", () => {
-    // Check that .git directory exists after cloning
-    const gitDirPath = path.join(projectDir, ".git");
-    const gitDirAfterClone = fs.existsSync(gitDirPath);
-    console.log(".git directory exists after clone:", gitDirAfterClone);
-    expect(gitDirAfterClone).to.be.true;
-
-    // Remove the .git directory
-    execSync(`rm -rf ${gitDirPath}`);
-
-    // Then, verify that .git directory no longer exists
-    const gitDirAfterRemoval = fs.existsSync(gitDirPath);
-    console.log(
-      ".git directory exists after removal attempt:",
-      gitDirAfterRemoval
-    );
-    expect(gitDirAfterRemoval).to.be.false;
+    try {
+      // Check that .git directory exists after cloning
+      const gitDirPath = path.join(tempProjectDirectory, ".git");
+      const gitDirAfterClone = fs.existsSync(gitDirPath);
+      console.log(".git directory exists after clone:", gitDirAfterClone);
+      expect(gitDirAfterClone).to.be.true;
+      // Remove the .git directory
+      execSync(`rm -rf ${gitDirPath}`);
+      // Then, verify that .git directory no longer exists
+      const gitDirAfterRemoval = fs.existsSync(gitDirPath);
+      console.log(
+        ".git directory exists after removal attempt:",
+        gitDirAfterRemoval
+      );
+      expect(gitDirAfterRemoval).to.be.false;
+    } catch (error) {
+      console.error("Error:", error);
+      throw error;
+    }
   });
 
   it("should initialize a new git repository in the cloned directory", () => {
     try {
-      execSync("git init", { cwd: projectDir });
-      const gitDirExists = fs.existsSync(path.join(projectDir, ".git"));
+      execSync("git init", { cwd: tempProjectDirectory });
+      const gitDirExists = fs.existsSync(
+        path.join(tempProjectDirectory, ".git")
+      );
       expect(gitDirExists).to.be.true;
     } catch (error) {
       console.error("Error:", error);
@@ -89,7 +93,7 @@ describe("init.js script", () => {
 
   it("should update package.json with the correct project name", () => {
     try {
-      const packageJsonPath = path.join(projectDir, "package.json");
+      const packageJsonPath = path.join(tempProjectDirectory, "package.json");
       const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
       expect(packageJson.name).to.equal(projectName);
     } catch (error) {
@@ -100,7 +104,7 @@ describe("init.js script", () => {
 
   it("should clear the author in package.json", () => {
     try {
-      const packageJsonPath = path.join(projectDir, "package.json");
+      const packageJsonPath = path.join(tempProjectDirectory, "package.json");
       const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
       expect(packageJson.author).to.equal("");
     } catch (error) {
@@ -109,5 +113,5 @@ describe("init.js script", () => {
     }
   });
 
-  after(cleanupProjectDir);
+  after(removeTempProjectDirectory);
 });
